32a39d25880714c35ebf3b80b6c0515746853cf8
ekimaycicek <ekimaycicek@icloud.com>
2023-09-19 21:09:12 +0300

    new

--- src/main/java/SallaryRunner.java
+++ src/main/java/SallaryRunner.java
@@ -8,7 +8,6 @@ public class SallaryRunner {
         System.out.println("Ödenecek vergi = " + employee.tax() + "$");
         System.out.println("ödenecek bonus = " + employee.bonus());
         System.out.println("employee.raiseSalary() = " + employee.raiseSalary());
-
         System.out.println("employee.salary+ employee.raiseSalary() = " + ((employee.salary) + (employee.raiseSalary())));
 
         System.out.println("employee.toString() = " + employee.toString());
