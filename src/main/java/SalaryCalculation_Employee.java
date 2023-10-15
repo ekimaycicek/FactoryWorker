a2039f6ce2f34f9f030812bc3c12623278547657
ekimaycicek <ekimaycicek@icloud.com>
2023-10-15 23:45:31 +0300

    new

--- src/main/java/SalaryCalculation_Employee.java
+++ src/main/java/SalaryCalculation_Employee.java
@@ -70,6 +70,7 @@ public class SalaryCalculation_Employee {
         return paymentOfBonus;
     }
 
+
     public double raiseSalary(){
         int timeOfYear=2021;
         int sumOfYear=timeOfYear-year;
